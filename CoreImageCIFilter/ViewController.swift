4fe3a7a8a71a8385e03b13f4554cb6e255c7df44
D.A Jalloh <info@codewithjalloh.com>
2016-07-27 22:17:06 +0100

    outlet and action added

--- CoreImageCIFilter/ViewController.swift
+++ CoreImageCIFilter/ViewController.swift
@@ -7,13 +7,50 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
+    var slierValue: Float = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    // outlet and action 
+    @IBOutlet weak var myImg: UIImageView!
+    
+    @IBAction func sepia(sender: UIButton) {
+        
+    }
+    
+    @IBAction func vignette(sender: UIButton) {
+        
+    }
+    
+    @IBAction func invert(sender: UIButton) {
+        
+    }
+    
+    @IBAction func photo(sender: UIButton) {
+        
+    }
+    
+    @IBAction func perspective(sender: UIButton) {
+        
+    }
+    
+    @IBAction func gussian(sender: UIButton) {
+        
+    }
+    
+    @IBAction func slider(sender: UISlider) {
+        slierValue = sender.value
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
